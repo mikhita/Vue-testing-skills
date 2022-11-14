5824b7b856ef90093dffdbd4a4f8e9d8a20a55fb
mikhita <mikheilispiriani@yahoo.com>
2022-11-14 09:37:29 +0400

    I complete counter with vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import {ref} from "vue"
+
+  const count = ref(0)
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>the current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    background-color: aqua;
+    height: 100vh;
+    width:100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  div{
+    text-align: center;
+  }
+  button{
+    width: 30px;
+    height: 30px;
+    margin: 10px;
+    font-size: larger;
+    color: red;
+  }
+</style>
